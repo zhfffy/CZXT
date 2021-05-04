ab22231e206b5e202b25565d6434f1543047bfad
zhfffy <564952043@qq.com>
2021-05-05 00:35:00 +0800

    lab 03

--- os/src/memory/mod.rs
+++ os/src/memory/mod.rs
@@ -1,4 +1,6 @@
-//! 内存模块
+//! 内存管理模块
+//!
+//! 负责空间分配和虚拟地址映射
 
 // 因为模块内包含许多基础设施类别，实现了许多以后可能会用到的函数，
 // 所以在模块范围内不提示「未使用的函数」等警告
@@ -6,21 +8,29 @@
 
 pub mod address;
 pub mod config;
-pub mod heap;
 pub mod frame;
+pub mod heap;
+pub mod mapping;
 pub mod range;
 
 pub use {
     config::*,
     address::*,
     frame::FRAME_ALLOCATOR,
+    mapping::{Flags, MapType, MemorySet, Segment},
     range::Range,
 };
 
 /// 一个缩写，模块中一些函数会使用
 pub type MemoryResult<T> = Result<T, &'static str>;
 
+/// 初始化内存相关的子模块
+///
+/// - [`heap::init`]
 pub fn init() {
     heap::init();
-    println!("Memory initialized.")
+    // 允许内核读写用户态内存
+    unsafe { riscv::register::sstatus::set_sum() };
+    
+    println!("Mod memory is initialized by Lingfeng Zhu.")
 }
